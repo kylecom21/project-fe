6e3996aa2af96f267462d0959a1ba9327c56c18c
Kyle Compton <kylecompton@hotmail.co.uk>
2024-10-15 14:51:44 +0100

    Content starting to be inserted

--- src/Components/Crypto.jsx
+++ src/Components/Crypto.jsx
@@ -1,8 +1,36 @@
-import { getCoinMarkets } from "../../api"
-import { useState, useEffect } from "react"
+import { Link } from "react-router-dom";
+import { getCoinMarkets } from "../../api";
+import { useState, useEffect } from "react";
 
-const Crypto = ()  => {
+const Crypto = () => {
+  const [coinMarkets, setCoinMarkets] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-}
+  useEffect(() => {
+    getCoinMarkets().then((coinMarkets) => {
+      setCoinMarkets(coinMarkets);
+      setIsLoading(false);
+    }),
+      [];
+  });
+  return isLoading ? (
+    <h2>Loading...</h2>
+  ) : (
+    <div className="coins">
+      {coinMarkets.map((coin) => {
+        return (
+        <Link to={`/crypto${coin.id}`} key={coin.id}>
+            <div className="coin-list">
+                <li className="coin-item" key={coin.id}>
+                    <p>{coin.market_cap_rank}</p>
+                    <p>{coin.name}</p>
+                    <p>{coin.current_price.toLocaleString()}</p>
+                </li>
+            </div>
+        </Link>
+      )})}
+    </div>
+  );
+};
 
-export default Crypto
+export default Crypto;
