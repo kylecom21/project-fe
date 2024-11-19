994a19b46f01fc55289ae9d55c063581640713bc
Kyle Compton <kylecompton@hotmail.co.uk>
2024-11-19 16:28:25 +0000

    back button added

--- src/Components/Crypto.jsx
+++ src/Components/Crypto.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { getCoinDetails } from "../../api";
 import formatMarketCap from "../Functions/formatMcap";
 import CryptoChart from "./CryptoChart";
@@ -7,6 +7,7 @@ import Statbox from "./CryptoStatBox";
 
 const Crypto = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
   const [coinData, setCoinData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
@@ -24,50 +25,57 @@ const Crypto = () => {
 
     fetchCoinData();
   }, [id]);
+  const handleBackClick = () => {
+    navigate(-1);
+  };
   const handleBuyClick = () => {
     if (coinData) {
       const buyUrl = `https://www.binance.com/en-GB/trade/${coinData.symbol.toUpperCase()}_USDT?type=spot`;
-      window.open(buyUrl, "_blank"); 
+      window.open(buyUrl, "_blank");
     }
   };
   return isLoading ? (
     <h2>Loading...</h2>
   ) : (
-<div className="coin-page">
-  <div className="coin-header">
-    <img
-      src={coinData.image.large}
-      alt={`${coinData.id} logo`}
-      className="coin-logo"
-    />
-    <h1 className="coin-name">{coinData.name}</h1>
-    <div className="percent-change-box">
-      <h2>{coinData.market_data.price_change_percentage_24h.toFixed(2)}%</h2>
+    <div className="coin-page">
+      <button className="direction-button" onClick={handleBackClick}>Back</button>
+      <div className="coin-header">
+        <img
+          src={coinData.image.large}
+          alt={`${coinData.id} logo`}
+          className="coin-logo"
+        />
+        <h1 className="coin-name">{coinData.name}</h1>
+        <div className="percent-change-box">
+          <h2>
+            {coinData.market_data.price_change_percentage_24h.toFixed(2)}%
+          </h2>
+        </div>
+      </div>
+
+      <div className="crypto-stats">
+        <p>
+          <strong>Price: </strong> $
+          {coinData.market_data.current_price.usd.toLocaleString()}
+        </p>
+        <p>
+          <strong>Market Cap: </strong>
+          {formatMarketCap(coinData.market_data.market_cap.usd)}
+        </p>
+        <p>
+          <strong>Volume: </strong>
+          {formatMarketCap(coinData.market_data.total_volume.usd)}
+        </p>
+      </div>
+
+      <CryptoChart coinId={coinData.id} />
+
+      <Statbox />
+
+      <button className="buy-button" onClick={handleBuyClick}>
+        Buy
+      </button>
     </div>
-  </div>
-  
-  <div className="crypto-stats">
-    <p>
-      <strong>Price: </strong> $
-      {coinData.market_data.current_price.usd.toLocaleString()}
-    </p>
-    <p>
-      <strong>Market Cap: </strong>
-      {formatMarketCap(coinData.market_data.market_cap.usd)}
-    </p>
-    <p>
-      <strong>Volume: </strong>
-      {formatMarketCap(coinData.market_data.total_volume.usd)}
-    </p>
-  </div>
-  
-    <CryptoChart coinId={coinData.id}/>
-
-    <Statbox/>
-    
-
-  <button className="buy-button" onClick={handleBuyClick}>Buy</button>
-</div>
   );
 };
 
