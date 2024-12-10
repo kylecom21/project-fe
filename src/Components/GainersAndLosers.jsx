b6352e92a1a5ba871c0e9d6d4c3bf6f2f12f24e2
Kyle Compton <kylecompton@hotmail.co.uk>
2024-12-10 18:18:42 +0000

    trying to limit gain and losers

--- src/Components/GainersAndLosers.jsx
+++ src/Components/GainersAndLosers.jsx
@@ -1,19 +1,22 @@
 import { useState, useEffect } from "react";
-import {gainersAndLosers} from "../../api";
+import { gainersAndLosers } from "../../api";
 
 const GainersAndLosers = () => {
   const [gainers, setGainers] = useState([]);
   const [losers, setLosers] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
+  const limitToThree = (array) => array.slice(0, 3);
+  
   useEffect(() => {
+
     const fetchGainersAndLosers = async () => {
       try {
         const data = await gainersAndLosers();
 
         if (data) {
-          setGainers(data.top_gainers.slice(0, 3));
-          setLosers(data.top_losers.slice(0, 3));
+          setGainers(limitToThree(data.top_gainers));
+          setLosers(limitToThree(data.top_losers));
         }
         setIsLoading(false);
       } catch (error) {
@@ -24,28 +27,32 @@ const GainersAndLosers = () => {
 
     fetchGainersAndLosers();
   }, []);
-  return <div className="gainers-losers-container">
-  <div className="gainers-box">
-    <h3>Top Gainers</h3>
-    <ul>
-      {gainers.map((stock, index) => (
-        <li key={index}>
-          <strong>{stock.ticker}</strong>: ${stock.price} - {stock.change_percentage}%
-        </li>
-      ))}
-    </ul>
-  </div>
-  <div className="losers-box">
-    <h3>Top Losers</h3>
-    <ul>
-      {losers.map((stock, index) => (
-        <li key={index}>
-          <strong>{stock.ticker}</strong>: ${stock.price} - {stock.change_percentage}%
-        </li>
-      ))}
-    </ul>
-  </div>
-</div>;
+  return (
+    <div className="gainers-losers-container">
+      <div className="gainers-box">
+        <h3>Top Gainers</h3>
+        <ul>
+          {gainers.map((stock, index) => (
+            <li key={index}>
+              <strong>{stock.ticker}</strong>: ${stock.price} -{" "}
+              {stock.change_percentage}%
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="losers-box">
+        <h3>Top Losers</h3>
+        <ul>
+          {losers.map((stock, index) => (
+            <li key={index}>
+              <strong>{stock.ticker}</strong>: ${stock.price} -{" "}
+              {stock.change_percentage}%
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default GainersAndLosers;
